935a2e1a6b260620e6fc73196fe3823ecbc2c1bc
rishathi <risha.thimmancherla@gmail.com>
2025-07-29 21:24:59 -0700

    'Qualifications' basic styling

--- src/pages/Qualifications.jsx
+++ src/pages/Qualifications.jsx
@@ -1,8 +1,88 @@
+import {
+  tools,
+  languages,
+  education,
+  experience,
+  recognition,
+} from "../utils/qualData";
+
+import "./Qualifications.css";
+
 function Qualifications() {
   return (
-    <div>
+    <div
+      className="qualifications"
+      style={{ marginLeft: "5vw", marginRight: "5vw" }}
+    >
       <h1>Qualifications</h1>
-      <p>This is the Qualifications page!</p>
+
+      <div className="qualification-content">
+        <div className="left-column">
+          <p className="qualifications-intro">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate.
+          </p>
+
+          <div className="tools-languages">
+            <div className="tools">
+              <ul>
+                {tools.map((tool, index) => (
+                  <li key={index}>{tool}</li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="languages">
+              <ul>
+                {languages.map((lang, index) => (
+                  <li key={index}>{lang}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="right-column">
+          <div className="education">
+            <h2>Education</h2>
+            <ul>
+              {education.map((school, index) => (
+                <li key={index}>
+                  <p>
+                    {school.school} - {school.degree}
+                  </p>
+                  <p>({school.minor})</p>
+                  <p>Class of {school.year}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="experience">
+            <h2>Experience</h2>
+            <ul>
+              {experience.map((job, index) => (
+                <li key={index}>
+                  {job.title} at {job.company} ({job.year})
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="recognition">
+            <ul>
+              {recognition.map((rec, index) => (
+                <li key={index}>
+                  <button className="button2">{rec}</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/utils/qualData.jsx
@@ -0,0 +1,72 @@
+export let tools = [
+  "Duda Certified in Web Design",
+  "Github (+copilot)",
+  " Figma",
+  "Jira (Agile Framework)",
+  "Slack",
+  "Adobe Illustrator",
+  "Adobe InDesign",
+  "Adobe XD",
+  "Adobe Photoshop",
+  "PostgreSQL",
+  // "Firebase",
+  // "AWS",
+  // "Google Cloud Platform",
+  "Notion",
+  "Microsoft Office Suite",
+  "Google Workspace",
+  "Canva",
+  "Visual Studio Code",
+];
+
+export let languages = [
+  "Java",
+  "Python",
+  "C++",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Node.js",
+  "Next.js",
+  "C#",
+  "HTML",
+  "CSS",
+  "JavaFX",
+  "SQL",
+  "Haskell",
+  "Bootstrap",
+  "Material UI",
+  "AG Grid",
+  "Zustand",
+];
+
+export let education = [
+  {
+    school: "Texas A&M University",
+    degree: "B.S. in Computer Science",
+    minor: "Minor in Graphic Design",
+    year: "2026",
+  },
+];
+
+export let experience = [
+  {
+    title: "Software Engineer Intern",
+    company: "MerQube",
+    year: "2025",
+  },
+  {
+    title: "Software Engineer Intern",
+    company: "BizAcuity",
+    year: "2025",
+  },
+];
+
+export let recognition = [
+  "Dean’s Honor Roll Certification",
+  "National Merit Scholar",
+  "President’s Endowed Scholarship Recipient",
+  "Engineering Honors Student",
+  "AP Scholar with Distinction",
+  "World Taekwondo Federation – Black Belt Certification",
+];
